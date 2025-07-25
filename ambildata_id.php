acc00447775a20edd4489ee6e555edd2450510a0
erikaazizahk-dev <erikaazizah.k@gmail.com>
2025-07-26 03:51:06 +0700

    Update ambildata_id.php

--- ambildata_id.php
+++ ambildata_id.php
@@ -5,21 +5,26 @@ include "koneksi.php";
 $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
 $posts = array();
-$data = array();
 
 if ($id > 0) {
-    // Query dengan prepared statement
-    $stmt = $koneksi->prepare("SELECT * FROM data__fix WHERE id_industri = ?");
-    $stmt->bind_param("i", $id);
-    $stmt->execute();
+    // Query gabungan dari dua tabel: data_fix_1 dan data_fix_2
+    $query = "
+        (SELECT * FROM data__fix_1 WHERE id_industri = ?)
+        UNION ALL
+        (SELECT * FROM data__fix_2 WHERE id_industri = ?)
+    ";
 
+    $stmt = $koneksi->prepare($query);
+    $stmt->bind_param("ii", $id, $id); // dua kali karena dua ? di query
+
+    $stmt->execute();
     $result = $stmt->get_result();
-    while($row = $result->fetch_assoc()){
+
+    while ($row = $result->fetch_assoc()) {
         $posts[] = $row;
     }
 
-    $data = json_encode(array('results' => $posts));
-    echo $data;
+    echo json_encode(array('results' => $posts));
 } else {
     echo json_encode(array('results' => []));
 }
